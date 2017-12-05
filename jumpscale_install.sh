5476c4a3bd6f5521cfaccb7bdc4cd9cd2c6e6969
Abdelrahman Hussein <ahussein.abdelrahman@gmail.com>
2017-12-05 01:20:30 +0100

    update jumpscale install

--- jumpscale_install.sh
+++ jumpscale_install.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 set -e
 
-# Install ays9 in a docker contianer using bash installers
-ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+# Install python3.5
+# sudo add-apt-repository -y ppa:deadsnakes/ppa
+# sudo apt-get update
+# sudo apt-get install -y python3.5
+
+# Instrall virtualenv
+# sudo apt-get install -y python-virtualenv
+# virtualenv -p python3.5 virtualenv
+# . ./virtualenv/bin/activate
+
+# Install python3.5-dev
+sudo apt-get install -y python3.5-dev
 
 export ZUTILSBRANCH=${ZUTILSBRANCH:-master}
+export JSBRANCH=${JSBRANCH:-master}
 
-curl https://raw.githubusercontent.com/Jumpscale/bash/$ZUTILSBRANCH/install.sh?$RANDOM > /tmp/install.sh;sudo -E bash /tmp/install.sh
-sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZCodeGetJS"
-eval $(ssh-agent)
-ssh-add
-sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZInstall_host_js9_full"
+echo "INSTALL BASHTOOLS"
+curl https://raw.githubusercontent.com/Jumpscale/bash/$ZUTILSBRANCH/install.sh?$RANDOM > /tmp/install.sh;bash /tmp/install.sh
+. /opt/code/github/jumpscale/bash/zlibs.sh
+echo "install js9"
+ZInstall_host_js9
+pip3 install Cython
+pip3 install asyncssh
+pip3 install numpy
+pip3 install tarantool
