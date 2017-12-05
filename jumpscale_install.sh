164e1bd47e8e50c75e62a47a90a00aa1263e5afa
Abdelrahman Hussein <husseina@greenitglobe.com>
2017-12-05 11:43:01 +0100

    update install script

--- jumpscale_install.sh
+++ jumpscale_install.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 set -e
 
-# Install python3.5
+# Install python3.5-dev
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get update
-# sudo apt-get install -y python3.5
+sudo apt-get install -y python3.5-dev
 
 # Instrall virtualenv
-# sudo apt-get install -y python-virtualenv
-# virtualenv -p python3.5 virtualenv
-# . ./virtualenv/bin/activate
-
-# Install python3.5-dev
-sudo apt-get install -y python3.5-dev
+sudo apt-get install -y python-virtualenv
+virtualenv -p python3.5 virtualenv
+. ./virtualenv/bin/activate
 
 export ZUTILSBRANCH=${ZUTILSBRANCH:-master}
 export JSBRANCH=${JSBRANCH:-master}
 
 echo "INSTALL BASHTOOLS"
 curl https://raw.githubusercontent.com/Jumpscale/bash/$ZUTILSBRANCH/install.sh?$RANDOM > /tmp/install.sh;sudo -EH bash /tmp/install.sh
-. /opt/code/github/jumpscale/bash/zlibs.sh
+ . /opt/code/github/jumpscale/bash/zlibs.sh
+
 echo "install js9"
 ZInstall_host_js9
+
 pip3 install Cython
 pip3 install asyncssh
 pip3 install numpy
