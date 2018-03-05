d0c394671e7a98ab4bfc30e2769e59728402e2d1
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2018-03-05 15:53:40 +0100

    rename `eco.toJSON` to `eco.json`
    fixes #115

--- zerorobot/template/base.py
+++ zerorobot/template/base.py
@@ -191,15 +191,13 @@ class TemplateBase:
             task = None
             try:
                 task = self.task_list.get()
-                started = time.time()
                 task.execute()
             except gevent.GreenletExit:
                 # TODO: gracefull shutdown
                 break
             finally:
                 if task:
-                    task._duration = time.time() - started
-                    task_latency.labels(action_name=task.action_name, template_uid=str(self.template_uid)).observe(task._duration)
+                    task_latency.labels(action_name=task.action_name, template_uid=str(self.template_uid)).observe(task.duration)
                     # notify the task list that this task is done
                     self.task_list.done(task)
 
