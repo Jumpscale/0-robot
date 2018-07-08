8d70268e77e22cc270d8cac2c4e042d9bbbdb273
BolaNasr <bola.e.nasr@gmail.com>
2018-07-08 17:09:30 +0200

    god_jwt to create, decode, encode and verify JWT using specific algorithm

---
+++ zerorobot/server/auth/god_jwt.py
@@ -0,0 +1,62 @@
+from functools import wraps
+import os
+
+from jose import jwt
+from js9 import j
+
+logger = j.logger.get('zrobot')
+_token_prefix = "Bearer "
+
+
+def create(claims):
+    """create a JWT with the claims pass as argument
+
+    Arguments:
+        claims {dict} -- dict of claims to include in the JWT
+    """
+    key = _get_key()
+    return jwt.encode(claims, key, algorithm='HS512') #using HS512 algorithm 
+
+
+def decode(token):
+    key = _get_key()
+    return jwt.decode(token, key, algorithms='HS512')
+
+
+def verify(token):
+    if not token:
+        return False
+
+    expected = {'god_token_':'GodToken'}
+    try:
+        claims = decode(token)
+        if claims == expected:
+            return True
+    except Exception as err:
+        logger.error('error decoding user secret: %s', str(err))
+
+    return False
+
+
+def _get_key():
+    """return the signing key to create JWT
+    the key is the one used by the config manager of JumpScale
+
+    Raises:
+        SigningKeyNotFoundError -- [raised if signin key is not found]
+
+    Returns:
+        str -- the signing key
+    """
+    if j.tools.configmanager.keyname is None or j.tools.configmanager.keyname == '':
+        raise SigningKeyNotFoundError('no key configured')
+
+    key_path = os.path.expanduser(os.path.join('~/.ssh', j.tools.configmanager.keyname))
+    if not os.path.exists(key_path):
+        raise SigningKeyNotFoundError('key not found')
+
+    return j.sal.fs.fileGetContents(key_path)
+
+
+class SigningKeyNotFoundError(Exception):
+    pass
